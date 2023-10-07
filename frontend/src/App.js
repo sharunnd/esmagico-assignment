c62d1dd48234bd7e53dda2c9d9d4057eadaeb0e8
Your Name <sharunnd11@gmail.com>
2023-10-07 20:20:01 +0530

    top section for mobile screens

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -17,13 +17,13 @@ function App() {
       <TopSection />
       <Navbar />
       <MiddleSection />
-      <LiveLearningSessions />
-      <LiveLearningSessionWork />
-      <VideoCarousel />
-      <Clients />
-      <About />
-      <TrendingSpeakers />
-      <Footer />
+      {/* <LiveLearningSessions /> */}
+      {/* <LiveLearningSessionWork /> */}
+      {/* <VideoCarousel /> */}
+      {/* <Clients /> */}
+      {/* <About /> */}
+      {/* <TrendingSpeakers /> */}
+      {/* <Footer /> */}
     </div>
   );
 }
