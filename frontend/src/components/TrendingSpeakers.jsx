5593a0a277315fa39d08a0547b5bd72e60e28901
Your Name <sharunnd11@gmail.com>
2023-10-08 15:00:09 +0530

    footer, registration form and trending speakers responsiveness

--- frontend/src/components/TrendingSpeakers.jsx
+++ frontend/src/components/TrendingSpeakers.jsx
@@ -1,3 +1,4 @@
+import React, { useState, useEffect } from "react";
 import { Box, Flex, Image, Text } from "@chakra-ui/react";
 import nameCard1 from "../images/namecard/NameCard1.png";
 import nameCard2 from "../images/namecard/NameCard2.png";
@@ -7,25 +8,47 @@ import nameCard5 from "../images/namecard/NameCard5.png";
 
 const TrendingSpeakers = () => {
   const nameCards = [nameCard1, nameCard2, nameCard3, nameCard4, nameCard5];
+  const [currentIndex, setCurrentIndex] = useState(0);
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setCurrentIndex((prevIndex) => (prevIndex + 1) % nameCards.length);
+    }, 1000); // Change slide every 3 seconds
+
+    return () => clearInterval(interval);
+  }, [nameCards.length]);
+
+  const isSmallScreen = window.innerWidth < 768;
+
   return (
-    <Box mt={{ base: "10px", sm: "20px", md: "25px", lg: "29px" }} textAlign="center" height={{ base: "222px", sm: "230px", md: "300px", lg: "345px" }}>
-      <Box
-        width={{ base: "250pxpx", sm: "250px", md: "305px", lg: "305px" }}
-        height="47px"
-        lineHeight={{ base: "23px", sm: "30px", md: "47px", lg: "47px" }}
-        justifyContent="center"
-        margin="0 auto"
-      >
-        <Text fontWeight={600} fontSize={{ base: "16px", sm: "20px", md: "32px", lg: "32px" }}>
-          Trending Speakers
-        </Text>
-        <Flex justifyContent="center" mt={{ base: "10px", sm: "30px", md: "40px", lg: "42px" }}>
-          {nameCards &&
-            nameCards.map((card, index) => (
-              <Image key={index} src={card} width={{ base: "222px", sm: "250px", md: "80%", lg: "100%" }} alt={`namecard-${index}`} />
-            ))}
+    <Box mt={{ base: "10px", sm: "20px", md: "25px", lg: "29px" }} textAlign="center">
+      <Text fontWeight={600} fontSize={{ base: "16px", sm: "20px", md: "32px", lg: "32px" }} mb="4">
+        Trending Speakers
+      </Text>
+      {isSmallScreen ? (
+        <Flex justifyContent="center" overflow="hidden">
+          {nameCards.map((card, index) => (
+            <Image
+              key={index}
+              src={card}
+              width={{ base: "222px", sm: "250px", md: "33.33%", lg: "33.33%" }}
+              alt={`namecard-${index}`}
+              style={{ transform: `translateX(${-currentIndex * 33.33}%)`, transition: "transform 0.5s ease-in-out" }}
+            />
+          ))}
         </Flex>
-      </Box>
+      ) : (
+        <Flex justifyContent="center">
+          {nameCards.map((card, index) => (
+            <Image
+              key={index}
+              src={card}
+              width={{ base: "142px", sm: "150px", md: "33.33%", lg: "33.33%" }}
+              alt={`namecard-${index}`}
+            />
+          ))}
+        </Flex>
+      )}
     </Box>
   );
 };
